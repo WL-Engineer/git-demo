b5d051a7b46ad775a169f6df8babb86feb9635e9
WL <wei_li@bupt.edu.cn>
2021-12-23 21:15:11 +0800

    Create README.md

--- mvn01/src/main/java/com/atguigu/dao/GitDao.java
+++ mvn01/src/main/java/com/atguigu/dao/GitDao.java
@@ -4,6 +4,9 @@ public class GitDao {
     public static void main(String[] args) {
         System.out.println("git");
     }
+    public void test001() {
+        System.out.println("为什么不行");
+    }
 
 
 }
